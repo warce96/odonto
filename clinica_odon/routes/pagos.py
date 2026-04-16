ed3f9e568dca0a19f6dfcb917df4f7be62184b0f
warce96 <willce96@gmail.com>
2026-04-16 11:11:52 -0300

    Update pagos.py

--- clinica_odon/routes/pagos.py
+++ clinica_odon/routes/pagos.py
@@ -1,34 +1,36 @@
 from app import app
-from flask import render_template, redirect
+from flask import render_template, redirect, request
 from flask_login import login_required
 from models import Cuota, db
 from datetime import date
 
+
+# ================= LISTADO =================
 @app.route("/pagos")
 @login_required
 def pagos():
 
     cuotas = Cuota.query.order_by(Cuota.fecha_vencimiento).all()
 
-    # actualizar estado automático
-    for c in cuotas:
-        if c.estado != "PAGADO":
-            if c.fecha_vencimiento < date.today():
-                c.estado = "VENCIDO"
-            else:
-                c.estado = "PENDIENTE"
-
-    db.session.commit()
-
     return render_template("pagos.html", cuotas=cuotas)
 
 
-@app.route("/pagar/<int:id>")
+# ================= CAMBIAR ESTADO =================
+@app.route("/cambiar_estado/<int:id>", methods=["POST"])
 @login_required
-def pagar(id):
+def cambiar_estado(id):
 
     cuota = Cuota.query.get(id)
-    cuota.estado = "PAGADO"
+
+    if not cuota:
+        return "Error", 404
+
+    # 🔁 TOGGLE REAL
+    if cuota.estado == "PAGADO":
+        cuota.estado = "PENDIENTE"
+    else:
+        cuota.estado = "PAGADO"
+
     db.session.commit()
 
     return redirect("/pagos")
